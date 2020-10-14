34ea75e2b1879ec9e4828affb1270be32ad79a5c
Gregory Zentkovich <gzentkovich@gmail.com>
2020-10-14 08:57:25 -1000

    add search to interface findByTitleContaining

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,6 +4,9 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
+
 @RestResource
 public interface PostRepository extends JpaRepository<Post, Long> {
+    public List<Post> findByTitleContaining(String title);
 }
