ba8e478c9af149601bf48522e51adf5867c2b594
Gregory Zentkovich <gzentkovich@gmail.com>
2020-10-14 12:48:47 -1000

    refactor...

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -32,13 +32,11 @@ public class DatabaseLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         authors.addAll(Arrays.asList(
-                new Author("Vanquish", "Tom", "Hardy", "password"),
-                new Author("theKid", "William", "Anthrum", "password"),
+                new Author("sholderness", "Sarah", "Holderness", "password"),
+                new Author("tbell", "Tom", "Bell", "password"),
                 new Author("flash", "Barry", "Allen", "password"),
                 new Author("penquin", "Oswald", "Cobblepott","password")
-
         ));
-
         authorRepository.saveAll(authors);
 
         IntStream.range(0,40).forEach(i->{
